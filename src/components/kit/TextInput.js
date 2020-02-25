9f0dda79d852eee86994865d83b4c7674d166ab4
harmyderoman <romangarmider@gmail.com>
2020-02-25 15:19:58 +0200

    adding styled-components to dependencies

---
+++ src/components/kit/TextInput.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const InputField = styled.input`
+  height: 50px;
+  border: 1px solid #cdcdcd;
+  background: white;
+  border-radius: 25px;
+  overflow: hidden;
+  font-size: 20px;
+  padding-left: 20px;
+  outline: none;
+`;
+
+const TextInput = ({ type , placeholder}) => {
+  return <InputField type={type} placeholder={placeholder}></InputField>;
+};
+
+export default TextInput;
